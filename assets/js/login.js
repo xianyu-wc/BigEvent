5cde92ba1189ddfae535d1784466ad7e92f6397e
Cheng <xianyu_wc@163.com>
2020-09-02 16:32:08 +0800

    login

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,7 +9,60 @@ $(function () {
         $('.login-box').show()
     })
 
+    var form = layui.form
+    var layer = layui.layer   //导入弹出层模块
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ],
+        repwd: function (value) {
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次输入的密码不一致'
+            }
+        }
+    })
+
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault();
+        var data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        $.ajax({
+            url: '/api/reguser',
+            type: 'POST',
+            data,
+            success: function (res) {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg('注册成功，请登录！')
+                // 模拟人的点击行为
+                $('#link_login').click()
+            }
+        })
+    })
 
 
-
+    $('#form_login').on('submit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            type: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg(res.message)
+                // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                localStorage.setItem('token', res.token)
+                // 跳转到后台主页
+                location.href = '/index.html'
+            }
+        })
+    })
 })
